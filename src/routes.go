01a9951a82970d6d04bcea323cfea76691f2e731
verovec <verove.clement@gmail.com>
2023-07-01 13:44:14 +0200

    gin project

---
+++ src/routes.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func getPodInfo(c *gin.Context) {
+	os_release, err_os_release := os.ReadFile("/etc/os-release")
+	if err_os_release != nil {
+		log.Println("Could not read os release file")
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"HOSTNAME":   os.Getenv("HOSTNAME"),
+		"HOME":       os.Getenv("HOME"),
+		"APP_ENV":    os.Getenv("APP_ENV"),
+		"OS_RELEASE": string(os_release),
+	})
+}
